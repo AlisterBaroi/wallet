7b15e14f4dbc1f75357947e371527c53537a743f
Alister Animesh Baroi <44337842+AlisterBaroi@users.noreply.github.com>
2024-06-09 16:52:20 +0600

    Create cards.py

---
+++ pages/cards.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Alister's Online Wallet - Cards", page_icon=":card:", layout="wide"
+)
+
+
+def main():
+
+    st.title("Cards")
+
+
+if __name__ == "__main__":
+    main()
