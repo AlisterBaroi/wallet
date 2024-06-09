48eeebc32d1672f3457761835f50ca3b13f2c79e
Alister Animesh Baroi <44337842+AlisterBaroi@users.noreply.github.com>
2024-06-09 16:50:28 +0600

    Create wallet.py

---
+++ wallet.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Alister's Online Wallet App", page_icon=":money_bag:", layout="wide"
+)
+
+
+def main():
+
+    st.title("Alister's Online Wallet App")
+
+
+
+if __name__ == "__main__":
+    main()
